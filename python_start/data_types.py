fd19619524bc3c7b576c959936a160ae7cef19a3
suyumbaevaaigerim <suyumbaevaaigerim@gmail.com>
2022-07-18 23:08:51 +0600

    python_basic

---
+++ python_start/data_types.py
@@ -0,0 +1,99 @@
+'''TIPY DDANNYH'''
+
+
+#izmenyaemye tipy dannyh:
+# list - spisok 
+# set - mnojestva
+# dict - slovari 
+
+'''
+ne izmenyaenye tipy dannnyh:
+tuple - kortej
+str - stroki 
+bool - logicheskiy tip dannyh/bulevyu tip (True/False)
+int - tsifry(integer)
+float - drobnye chisla
+complex - slojnye chisla
+frozenset - ne izmenyaemoe mnojesrvo
+NoneType - tip dannyh pustogo znacheniya(None)
+'''
+
+'''
+peremennye
+'''
+#nazvaniye peremennoi ne doljno nachinat'sya s tsifr ili simvolov
+#num = 10 OK
+# 1num = Error - syntaxError
+# %,$,&,/, $num = 10 - Error
+
+
+# esli nazvanie peremennoi sostoit iz neskol'lix slov stoit razdelyat' _ (underscore)
+
+# my_num = 33 #OK - snake_case
+# myNum = 34 #OK - camelCase
+
+# my%num = 30 Error 
+# my num = 42 Error
+
+# nazvaniye peremennyx ne doljny sovpadat' s naxvaniyami kluchevyx slov.
+# print = 32 #Error
+
+# registr v pitone igraet vajnuyu rol'
+# a = 10
+# A = 20
+
+#peremennaya sostoyashaya iz zaglavnyx bukv yavlyautsya   ne izmenyaemymi/postoyannymi
+# URL = 'google.com'
+
+# xoroshaya practika: nazyvat' peremennye tak chhtob byylo ponyatno, chto oni xranyat
+# literaly - simvoly slujashie dlya opredeleniya dannyx
+
+# num = 10
+# my_float = 3.2
+# string = 'walking'
+# list_of_nums = [1,2,3]
+# tuple_of_nums = (1,2,3)
+# set_of_nums = {2, 3.6 , 6}
+# dictionary = {'word': 'slovo'}
+# none_type = None
+# true = True
+# false = False
+
+'''
+mnogostrochnye commentarii
+dlya napisaniya documentazii
+'''
+
+# odnostrochnye comentarii - isp dlya kratkogo opisaniya
+
+'''funksii print() input() type() id()
+'''
+
+# print('hello world')
+# a =  'hello world'
+# print(a)
+
+# print() - funksiya dlya vyvoda inf v terminal
+# input() - finksiya dlya vvoda inf iz terminala
+
+# string = input()
+# print(string)
+# age = input('your age?: ')
+# print('your age :' ,age)
+
+# age = int(input('your age?: '))
+# print('your age :' , age)
+
+# a = 10
+# str(a) # '10'
+# float(a) # 10.0
+
+# num = 22
+# print(type(num))
+
+# type() - funkziya dlya vyvoda tipa peredannyx dannyx
+# a = 10
+# b = 10
+# print(id(a))
+# print(id(b))
+
