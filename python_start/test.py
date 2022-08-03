dca3ecb880ad02c7644051930e0eee913b742903
suyumbaevaaigerim <suyumbaevaaigerim@gmail.com>
2022-08-03 21:38:00 +0600

    test

--- python_start/test.py
+++ python_start/test.py
@@ -1,7 +1,7 @@
 
 #task24
-string1 = "America" 
-string2 = "Japan" #"AJrpan"
+# string1 = "America" 
+# string2 = "Japan" #"AJrpan"
 # print(f'{string1[0]}{string2[0]}{string1[3]}{string2[2:]}')
 
 
@@ -29,3 +29,62 @@ string2 = "Japan" #"AJrpan"
 # else:
 #     print('no')
 
+
+
+# name_of_list = ['helloworld!']
+# string = name_of_list[0]
+# half_of_string = len(string) // 2 + 1
+# if not len(string) % 2 == 0:
+#     new_list = list(string[half_of_string:] + string[0:half_of_string])
+# else:
+#     new_list = list(string[half_of_string+ 1:] + string[0:half_of_string+ 1])
+# print(new_list)
+
+
+'''Создать три числа в списке list_.
+Вывести на экран yes, если среди них есть одинаковые, иначе вывести ERROR.
+Например, для списка [1, 1, 3], вывод будет:
+
+yes 
+а для списка [1, 2, 3]:
+
+'''
+
+# list_ = [5, 6, 44]
+# TASK 14
+# counter1 = list_.count(list_[0])
+# counter2 = list_.count(list_[1])
+
+# if counter1 == 2 or counter2 or counter1 == 3:
+#     print('yes')
+# else:
+#     print('ERROR')
+
+
+# list1 = [1, 2, 3, 3, 5, 6, 7,3]
+# list2 = [8, 9, 12, 1, 3, 4, 5]
+# res = []
+# #[1, 3 ,5]
+
+# # for i in list1:
+# #     for j in list2:
+# #         if i == j:
+# #             res.append(i)
+# # print(res)
+# i = 0
+# j = 0
+
+# while i != len(list1):
+#     while j != len(list2):
+#         if list1[i] == list2[j]:
+
+
+# list1 = ['world','hello']
+
+# new_list = list1[::-1]
+# print(new_list)
+
+# a = {'a': 1, 'b': 2, 'c': 1} 
+
+# for k , v in a.items():
+#     print(k,v)
